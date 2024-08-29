82daa96d4ea48ebcf6a16b0e5a6f81ac92c2addf
Lavicka13 <luky.lavicka@gmail.com>
2024-08-29 09:05:35 +0200

    Add files via upload

---
+++ LukasQuiz/LukasQuiz/Spieler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LukasQuiz
+{
+    public class Spieler
+    {
+        private int SpielerID;
+        private string Name;
+
+        public Spieler (int SpielerID, string Name)
+        {
+            this.SpielerID = SpielerID;
+            this.Name = Name;
+        }
+        public int SpielerID1 { get => SpielerID; set => SpielerID = value; }
+        public string Name1 { get => Name; set => Name = value; }
+    }
+}
